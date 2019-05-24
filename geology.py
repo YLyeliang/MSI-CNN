c3a3edfa8edcdd90ca8868c6480be2da8015eaa2
YLyeliang <k87974@163.com>
2019-05-24 17:35:22 +0800

    a little changes to the model

--- geology.py
+++ geology.py
@@ -1,10 +1,13 @@
 import cv2
 from PIL import Image
 import os
-from skimage.feature import hog,local_binary_pattern
+from skimage.feature import hog, local_binary_pattern
 from skimage import io
 from skimage.color import rgb2gray
-path="/home/yel/data/Aerialgoaf/detection/train"
+import matplotlib.pyplot as plt
+import numpy as np
+
+path = "/home/yel/data/Aerialgoaf/detection/train"
 
 # cap=cv2.VideoCapture(0)
 # while(1):
@@ -13,56 +16,215 @@ path="/home/yel/data/Aerialgoaf/detection/train"
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
 #         break
 
-files =os.listdir('./images')
+files = os.listdir('./images')
+
 
 # def LBP_visual(img):
 
 
+def areaCal(contour):
+    area = 0
+    for i in range(len(contour)):
+        area += cv2.contourArea(contour[i])
+    return area
+
+def histogram(img, rho=0.22):
+    """Calculate histogram and return the threshold that occupy top rho percent of histogram."""
+    hist, bins = np.histogram(img.ravel(), 255, [0, 255])
+    # hist=cv2.calcHist(img,[0],None,[256],[0,256])
+    total_pixel = img.size
+    count = 0
+    thresh = 0
+    for index, i in enumerate(hist):
+        count += i
+        if count / total_pixel > rho:
+            thresh = index
+            break
+    print("the threshold value:", thresh)
+    return thresh
+    # cv2.imshow("hist",hist)
+
+
 def HOG_visual(img_path):
-    img=io.imread(img_path)
-    img =rgb2gray(img)
-    cv2.imshow("origin",img)
+    img = io.imread(img_path)
+    img = rgb2gray(img)
+    cv2.imshow("origin", img)
     normalised_blocks, hog_image = hog(img, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(8, 8),
                                        visualize=True)
-    cv2.imshow("hog",hog_image)
+    cv2.imshow("hog", hog_image)
     cv2.waitKey()
 
 
 def LBP_visual(img_path):
-    img = io.imread(img_path,as_gray=True)
-    cv2.imshow("origin",img)
-    lbp=local_binary_pattern(img,9,5.0)
-    cv2.imshow("lbp",lbp)
+    img = io.imread(img_path, as_gray=True)
+    cv2.imshow("origin", img)
+    lbp = local_binary_pattern(img, 9, 5.0)
+    cv2.imshow("lbp", lbp)
     cv2.waitKey()
+
 
 def thresh_visual(img):
     cv2.imshow("src image", img)
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     cv2.namedWindow("threshold")
     cv2.createTrackbar("num", "threshold", 0, 255, lambda x: None)
+    # plt.hist(img.ravel(),256,[0,256])
+    # plt.show()
+    thresh_val = histogram(img_gray)
     while True:
-        num = cv2.getTrackbarPos("num","threshold")
-        _,thresh = cv2.threshold(img_gray,num,255,cv2.THRESH_BINARY)
-        cv2.imshow("threshold",thresh)
-        k=cv2.waitKey(1) & 0xFF
-        if k ==27:
+        num = cv2.getTrackbarPos("num", "threshold")
+        _, thresh = cv2.threshold(img_gray, num, 255, cv2.THRESH_BINARY)
+        cv2.imshow("threshold", thresh)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
             break
+        elif k == ord('e'):
+            # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            # cv2.drawContours(img_gray,contours,-1,(0,0,255),1)
+            # cv2.contourArea(contours,)
+            contours = remove_contours(thresh)
+            cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
+            cv2.imshow("contours", img)
+            cv2.waitKey()
 
 def sobel(img):
-    img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    img_edge=cv2.Canny(img,50,70)
-    cv2.imshow("sobel",img_edge)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img_edge = cv2.Canny(img, 50, 70)
+    cv2.imshow("sobel", img_edge)
     cv2.waitKey()
 
+def remove_contours(img):
+    channels = list(img.shape)
+    if len(channels) > 2:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    thresh_val = histogram(img, 0.2)
+    _, thresh = cv2.threshold(img, thresh_val, 255, cv2.THRESH_BINARY)
+    cv2.imshow("threshold", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_keep = []
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+        if area > 30 and area < 3000:
+            contours_keep.append(contours[i])
+    # cv2.drawContours(img, contours_keep, -1, (0, 0, 255), 1, )
+    # cv2.imshow("contours", img)
+    return contours_keep
+
+def rect_save(rect, ratio=1.5, dist=10):
+    rect_tmp = []
+    for i in rect:
+        center, size, theta = i
+        w, h = size
+        if w / h > ratio or h / w > ratio:
+            box = np.int0(cv2.boxPoints(i))
+            rect_tmp.append(box)
+
+    # for i, j in enumerate(rect_tmp):
+    #     center, size, theta = j
+    #     center = np.array(center)
+    #     dis = np.linalg.norm()
+    return rect_tmp
+
+def hsv_thresh(img):
+    """transfer rgb format into hsv format and perform inRange function to get
+    thresh image.
+    PS:Useless.
+    """
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    cv2.namedWindow("inRange")
+    cv2.createTrackbar("low", "inRange", 0, 255, lambda x: None)
+    cv2.createTrackbar("up", "inRange", 0, 255, lambda x: None)
+    while True:
+        low = cv2.getTrackbarPos("low", "inRange")
+        up = cv2.getTrackbarPos("up", "inRange")
+        lower = np.array([low, low, low])
+        upper = np.array([up, up, up])
+        thresh = cv2.inRange(img_hsv, lower, upper)
+        cv2.imshow("inRange", thresh)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:
+            break
+
+def kmeans(img):
+    # K-means聚类
+    # img = cv2.resize(src,(512,512))
+    Z = img.reshape((-1, 3))
+    Z = np.float32(Z)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 7
+    ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+    gray= cv2.cvtColor(res2,cv2.COLOR_BGR2GRAY)
+    unique=np.unique(gray)
+    _,thresh=cv2.threshold(gray,unique[0],255,cv2.THRESH_BINARY)
+    cv2.imshow("threshold", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours_keep = []
+    for i in range(len(contours)):
+        area = cv2.contourArea(contours[i])
+        if area > 20 and area < 5000:
+            contours_keep.append(contours[i])
+    minrect=minAreaRect(contours_keep)
+    rect=rect_save(minrect)
+    cv2.drawContours(img, rect, -1, (0, 255, 0), 1)
+    cv2.imshow("contours", img)
+    cv2.waitKey()
+    # cv2.imshow('res2', res2)
+    # cv2.imshow("origin",img)
+    # cv2.imshow("gray",gray)
+    # cv2.imshow("mask",mask)
+    # mask2=cv2.dilate(mask,cv2.getStructuringElement(cv2.MORPH_RECT,(9,9)))
+    # mask2=cv2.erode(mask2,cv2.getStructuringElement(cv2.MORPH_RECT,(9,9)))
+    # contours,hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    # image2=cv2.drawContours(img,contours,-1,(0,0,255),1)
+    # cv2.imshow("contour",image2)
+    # cv2.imshow("mask2",mask2)
+    # print(areaCal(contours))
+
+def minAreaRect(contours):
+    rect_keep = []
+    for i in contours:
+        rect = cv2.minAreaRect(i)
+        rect_keep.append(rect)
+    return rect_keep
+
 for src in files:
-    path = os.path.join('./images',src)
-    img =cv2.imread(os.path.join('./images',src))
+    path = os.path.join('./images', src)
+    img = cv2.imread(os.path.join('./images', src))
+    cv2.imshow("srouce", img)
+    kmeans(img)
+    # remove_contours(img)
+    # hsv_thresh(img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    contours_keep = remove_contours(img)
+    rect_keep = []
+    for i in contours_keep:
+        rect = cv2.minAreaRect(i)
+        rect_keep.append(rect)
+    # rect_save(rect_keep)
+    box_keep = []
+    for i in rect_keep:
+        center, size, theta = i
+        w, h = size
+        if w / h > 2 or w / h < 0.5:
+            box = np.int0(cv2.boxPoints(i))
+            box_keep.append(box)
+    # dst=cv2.equalizeHist(img)       #均衡化
+    # th_val=histogram(dst)
+    # _,thresh=cv2.threshold(dst,th_val,255,cv2.THRESH_BINARY)
+    # contours=remove_contours(thresh)
+    # cv2.drawContours(img_src,contours,-1,(0,0,255),1)
+    # cv2.imshow("equaHist",dst)
+    # cv2.imshow("contours",img)
+    # cv2.drawContours(img, contours_keep, -1, (0, 0, 255), 1)
+    cv2.drawContours(img, box_keep, -1, (0, 255, 0), 1)
+    # cv2.imshow("contours", img)
+    cv2.waitKey()
     # thresh_visual(img)
-    # HOG_visual(img)
-    sobel(img)
+    # HOG_visual(path)
+    # sobel(img)
     # LBP_visual(path)
     # thresh_adap=cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,13,2)
     # cv2.imshow("threshold_adap",thresh_adap)
-
-
-
--- network/msi_max.py
+++ network/msi_max.py
@@ -1,132 +1,133 @@
 import tensorflow as tf
 import numpy as np
-from core.utilities import conv_with_bn,dense_block,\
-    atrous_SPP,deconv_layer,crack_refine,max_pool,msra_initializer,_variable_on_gpu
-
+from core.utilities import conv_with_bn, dense_block, \
+    atrous_SPP, deconv_layer, crack_refine, max_pool, msra_initializer, _variable_on_gpu
 
 
 class msi_cnn(object):
     """Multi-scale input CNN framework for feature extraction"""
 
-    def __init__(self,inputs,phase_train):
-        self.outputs = self.forward(inputs,phase_train)
+    def __init__(self, inputs, phase_train):
+        self.outputs = self.forward(inputs, phase_train)
 
-    def forward(self,inputs,phase_train):
-        batch_size= tf.shape(inputs)[0]
-        norm1 = inputs/255.
+    def forward(self, inputs, phase_train):
+        batch_size = tf.shape(inputs)[0]
+        norm1 = inputs / 255.
         # norm1 = batch_norm_layer(inputs, phase_train, scope='norm1')
         norm2 = tf.image.resize_bilinear(norm1, (128, 128))
         norm3 = tf.image.resize_bilinear(norm1, (64, 64))
         norm4 = tf.image.resize_bilinear(norm1, (32, 32))
         # input_dense_block_1
         with tf.variable_scope('input_block_1'):
-            conv1 = dense_block(norm1, layers=4, growth_rate=24, phase_train=phase_train)  # 256x256
-            conv1 = conv_with_bn(conv1, [1, 1, 64], phase_train, True,
-                                       name='transition')  # trainsition
+            self.conv1 = dense_block(norm1, layers=4, growth_rate=24, phase_train=phase_train)  # 256x256
+            self.conv1 = conv_with_bn(self.conv1, [1, 1, 64], phase_train, True,
+                                      name='transition')  # trainsition
             # pool1
-            pool1 = max_pool(conv1,'pool1')  # 128x128
+            self.pool1 = max_pool(self.conv1, 'pool1')  # 128x128
         # input_dense_block_2
         with tf.variable_scope('input_block_2'):
-            conv2 = dense_block(norm2, layers=4, growth_rate=24, phase_train=phase_train)  # 128x128
-            conv2 = conv_with_bn(conv2, [1, 1, 64], phase_train, True,
-                                       name='transition')  # trainsition
+            self.conv2 = dense_block(norm2, layers=4, growth_rate=24, phase_train=phase_train)  # 128x128
+            self.conv2 = conv_with_bn(self.conv2, [1, 1, 64], phase_train, True,
+                                      name='transition')  # trainsition
 
         # dense_block_1
         with tf.variable_scope('dense_block_1'):
-            dense1 = tf.concat([pool1, conv2], axis=3)
-            dense1 = dense_block(dense1, layers=4, growth_rate=24, phase_train=phase_train)
-            dense1 = conv_with_bn(dense1, [1, 1, 64], phase_train, True,
-                                        name='transition')
+            self.dense1 = tf.concat([self.pool1, self.conv2], axis=3)
+            self.dense1 = dense_block(self.dense1, layers=4, growth_rate=24, phase_train=phase_train)
+            self.dense1 = conv_with_bn(self.dense1, [1, 1, 64], phase_train, True,
+                                       name='transition')
             # pool2
-            pool2 = max_pool(dense1, 'pool2')  # 64x64
+            self.pool2 = max_pool(self.dense1, 'pool2')  # 64x64
         # input_dense_block_3
         with tf.variable_scope('input_block_3'):
-            conv3 = dense_block(norm3, layers=4, growth_rate=24, conv_size=(3, 3), phase_train=phase_train)  # 64x64
-            conv3 = conv_with_bn(conv3, [1, 1, 64], phase_train, True,
-                                       name='transition')  # trainsition
+            self.conv3 = dense_block(norm3, layers=4, growth_rate=24, conv_size=(3, 3),
+                                     phase_train=phase_train)  # 64x64
+            self.conv3 = conv_with_bn(self.conv3, [1, 1, 64], phase_train, True,
+                                      name='transition')  # trainsition
         # dense_block_2
         with tf.variable_scope('dense_block_2'):
-            dense2 = tf.concat([pool2, conv3], axis=3)
-            dense2 = dense_block(dense2, layers=4, growth_rate=24, phase_train=phase_train)
-            dense2 = conv_with_bn(dense2, [1, 1, 64], phase_train, True,
-                                        name='transition')
+            self.dense2 = tf.concat([self.pool2, self.conv3], axis=3)
+            self.dense2 = dense_block(self.dense2, layers=4, growth_rate=24, phase_train=phase_train)
+            self.dense2 = conv_with_bn(self.dense2, [1, 1, 64], phase_train, True,
+                                       name='transition')
             # pool3
-            pool3 = max_pool(dense2,'pool3')
+            self.pool3 = max_pool(self.dense2, 'pool3')
         # input_dense_block_4
         with tf.variable_scope('input_block_4'):
-            conv4 = dense_block(norm4, layers=4, growth_rate=24, conv_size=(3, 3), phase_train=phase_train)  # 32x32
-            conv4 = conv_with_bn(conv4, [1, 1, 64], phase_train, True,
-                                       name='transition')  # trainsition
+            self.conv4 = dense_block(norm4, layers=4, growth_rate=24, conv_size=(3, 3),
+                                     phase_train=phase_train)  # 32x32
+            self.conv4 = conv_with_bn(self.conv4, [1, 1, 64], phase_train, True,
+                                      name='transition')  # trainsition
         # dense_block_3
         with tf.variable_scope('dense_block_3'):
-            dense3 = tf.concat([pool3, conv4], axis=3)
-            dense3 = dense_block(dense3, layers=4, growth_rate=24, phase_train=phase_train)
-            dense3 = conv_with_bn(dense3, [1, 1, 64], phase_train, True,
-                                        name='transition')
+            self.dense3 = tf.concat([self.pool3, self.conv4], axis=3)
+            self.dense3 = dense_block(self.dense3, layers=4, growth_rate=24, phase_train=phase_train)
+            self.dense3 = conv_with_bn(self.dense3, [1, 1, 64], phase_train, True,
+                                       name='transition')
             # pool4
-            pool4 = max_pool(dense3, 'pool4')  # 16x16
+            self.pool4 = max_pool(self.dense3, 'pool4')  # 16x16
         with tf.variable_scope('atrous_SPP'):
-            pool4 = atrous_SPP(pool4, phase_train)
-            pool4 = conv_with_bn(pool4, [1, 1, 256], phase_train, True,name='trainsition')
+            self.pool4 = atrous_SPP(self.pool4, phase_train)
+            self.pool4 = conv_with_bn(self.pool4, [1, 1, 256], phase_train, True, name='trainsition')
 
         """ End of encoder """
         """ start upsample """
         # upsample4
         # Need to change when using different dataset out_w, out_h
-        upsample4 = deconv_layer(pool4, [3, 3, 256, 256], [batch_size, 32, 32, 256], 2, "up4")
+        self.upsample4 = deconv_layer(self.pool4, [3, 3, 256, 256], [batch_size, 32, 32, 256], 2, "up4")
         # decode 4
         with tf.variable_scope('decode_4'):
-            cr4 = crack_refine(dense3, (3, 3), phase_train=phase_train)
-            concat4 = tf.concat([upsample4, cr4], axis=3)
-            conv_decode4 = conv_with_bn(concat4, [3, 3, 128], phase_train,
-                                              False, name="conv_decode4")
+            self.cr4 = crack_refine(self.dense3, (3, 3), phase_train=phase_train)
+            self.concat4 = tf.concat([self.upsample4, self.cr4], axis=3)
+            self.conv_decode4 = conv_with_bn(self.concat4, [3, 3, 128], phase_train,
+                                             False, name="conv_decode4")
 
         # upsample 3
-        upsample3 = deconv_layer(conv_decode4, [3, 3, 128, 128], [batch_size, 64, 64, 128], 2, "up3")
+        self.upsample3 = deconv_layer(self.conv_decode4, [3, 3, 128, 128], [batch_size, 64, 64, 128], 2, "up3")
         # decode 3
         with tf.variable_scope('decode_3'):
-            cr3 = crack_refine(dense2, (5, 5), phase_train=phase_train)
-            concat3 = tf.concat([upsample3, cr3], axis=3)
-            conv_decode3 = conv_with_bn(concat3, [3, 3,  64], phase_train, False,
-                                              name="conv_decode3")
+            self.cr3 = crack_refine(self.dense2, (5, 5), phase_train=phase_train)
+            self.concat3 = tf.concat([self.upsample3, self.cr3], axis=3)
+            self.conv_decode3 = conv_with_bn(self.concat3, [3, 3, 64], phase_train, False,
+                                             name="conv_decode3")
 
         # upsample2
-        upsample2 = deconv_layer(conv_decode3, [3, 3, 64, 64], [batch_size, 128, 128, 64], 2, "up2")
+        self.upsample2 = deconv_layer(self.conv_decode3, [3, 3, 64, 64], [batch_size, 128, 128, 64], 2, "up2")
         # decode 2
         with tf.variable_scope('decode_2'):
-            cr2 = crack_refine(dense1, (7, 7), phase_train=phase_train)
-            concat2 = tf.concat([upsample2, cr2], axis=3)
-            conv_decode2 = conv_with_bn(concat2, [3, 3,  64], phase_train, False,
-                                              name="conv_decode2")
+            self.cr2 = crack_refine(self.dense1, (7, 7), phase_train=phase_train)
+            self.concat2 = tf.concat([self.upsample2, self.cr2], axis=3)
+            self.conv_decode2 = conv_with_bn(self.concat2, [3, 3, 64], phase_train, False,
+                                             name="conv_decode2")
 
         # upsample1
-        upsample1 = deconv_layer(conv_decode2, [3, 3, 64, 64], [batch_size, 256, 256, 64], 2, "up1")
+        self.upsample1 = deconv_layer(self.conv_decode2, [3, 3, 64, 64], [batch_size, 256, 256, 64], 2, "up1")
         # decode4
         with tf.variable_scope('decode_1'):
-            cr1 = crack_refine(conv1, (9, 9), phase_train=phase_train)
-            concat1 = tf.concat([upsample1, cr1], axis=3)
-            conv_decode1 = conv_with_bn(concat1, [3, 3,  64], phase_train, False,
-                                              name="conv_decode1")
+            self.cr1 = crack_refine(self.conv1, (9, 9), phase_train=phase_train)
+            self.concat1 = tf.concat([self.upsample1, self.cr1], axis=3)
+            self.conv_decode1 = conv_with_bn(self.concat1, [3, 3, 64], phase_train, False,
+                                             name="conv_decode1")
         """ end of Decode """
 
-        return conv_decode1
+        return self.conv_decode1
+
 
 class msi_net(object):
-    def __init__(self,num_classes):
-        self.NUM_CLASSES=num_classes
+    def __init__(self, num_classes):
+        self.NUM_CLASSES = num_classes
 
-
-    def inference(self,inputs,phase_train):
-        conv_decode1 = msi_cnn(inputs,phase_train).outputs
+    def inference(self, inputs, phase_train):
+        conv_decode1 = msi_cnn(inputs, phase_train).outputs
         with tf.variable_scope('conv_classifier') as scope:
-            kernel = _variable_on_gpu('weights',shape=[1, 1, 64, self.NUM_CLASSES],initializer=msra_initializer(1, 64))
+            kernel = _variable_on_gpu('weights', shape=[1, 1, 64, self.NUM_CLASSES],
+                                      initializer=msra_initializer(1, 64))
             conv = tf.nn.conv2d(conv_decode1, kernel, [1, 1, 1, 1], padding='SAME')
             biases = _variable_on_gpu('biases', [self.NUM_CLASSES], tf.constant_initializer(0.0))
             logit = tf.nn.bias_add(conv, biases, name=scope.name)
         return logit
 
-
-    def weighted_loss(self,logits, labels, num_classes, head=None):
+    def weighted_loss(self, logits, labels, num_classes, head=None):
         """ median-frequency re-weighting """
         with tf.name_scope('loss'):
             logits = tf.reshape(logits, (-1, num_classes))
@@ -153,7 +154,7 @@ class msi_net(object):
 
         return loss
 
-    def loss_layer(self,logits,labels):
+    def loss_layer(self, logits, labels):
         loss_weight = np.array([
             0.2595,
             13.3899])  # class 0~11
@@ -163,9 +164,6 @@ class msi_net(object):
         return self.weighted_loss(logits, labels, num_classes=self.NUM_CLASSES, head=loss_weight)
 
 
-
-
-
-x=tf.placeholder(tf.float32,shape=[2,256,256,3])
+x = tf.placeholder(tf.float32, shape=[2, 256, 256, 3])
 phase_train = tf.placeholder(tf.bool, name='phase_train')
-model=msi_cnn(x,phase_train).outputs
+model = msi_cnn(x, phase_train).outputs
